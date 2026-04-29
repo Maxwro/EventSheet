482cdf1bd21a5fe39bce8f125f9d0b2fec08bbb6
Maxwro <glenn-andre_iversen@hotmail.com>
2026-04-29 10:45:50 +0200

    Add files via upload

--- EventSheet.js
+++ EventSheet.js
@@ -10,12 +10,13 @@ const eventLocation = document.getElementById('event-location')
 const previewScreen = document.getElementById('preview-screen')
 const briefingContent = document.getElementById('briefing-content')
 const backBtn = document.getElementById('back-btn')
-
+const downloadBtn = document.getElementById('download-btn')
 
 addFightBtn.addEventListener('click', function() {
     const fightBlock = document.createElement('div')
     fightBlock.innerHTML = '<label>Fight Location</label><input type="text" class="fight-location" placeholder="Where does the fight happen?">' +
-        '<label>Enemies</label><input type="text" class="fight-enemies" placeholder="How many enemies?">' +
+        '<label>Enemy type</label><input type="text" class="fight-enemies" placeholder="Who are you fighting?">' +
+        '<label>Enemy count</label><input type="text" class="fight-enemy-count" placeholder="How many enemies?">' +
         '<label>Enemy Description</label><textarea class="fight-enemy-desc" placeholder="Describe the enemies"></textarea>' +
         '<label>Fight Result</label><textarea class="fight-result" placeholder="What should the outcome be?"></textarea>'
     const removeBtn = document.createElement('button')
@@ -40,20 +41,40 @@ previewBtn.addEventListener('click', function() {
         var fight = fights[i]
         var fightLocation = fight.querySelector('.fight-location').value
         var fightEnemies = fight.querySelector('.fight-enemies').value
+        var fightEnemyCount = fight.querySelector('.fight-enemy-count').value
         var fightEnemyDesc = fight.querySelector('.fight-enemy-desc').value
         var fightResult = fight.querySelector('.fight-result').value
         fightHTML = fightHTML + '<h3>Fight ' + (i + 1) + '</h3>' +
         '<p>Location: ' + fightLocation + '</p>' +
         '<p>Enemies: ' + fightEnemies + '</p>' +
+        '<p>Count: ' + fightEnemyCount + '</p>' +
         '<p>Description: ' + fightEnemyDesc + '</p>' +
         '<p>Result: ' + fightResult + '</p>'
     }
+    var writerHTML = ''
+    if (writer !== '') {
+        writerHTML = '<p>Written by: ' + writer + '</p>'
+    }
+
     briefingContent.innerHTML = '<h1>' + title + '</h1>' +
     '<p>Location: ' + location + '</p>' +
     '<p>' + description + '</p>' +
     fightHTML +
     '<p>Created by: ' + creator + '</p>' +
-    '<p>Written by: ' + writer + '</p>'
+    writerHTML
     formScreen.hidden = true
     previewScreen.hidden = false
+})
+backBtn.addEventListener('click', function(){
+    previewScreen.hidden = true
+    formScreen.hidden = false
+})
+downloadBtn.addEventListener('click', function() {
+    var wrapper = document.getElementById('briefing-wrapper')
+    domtoimage.toJpeg(wrapper).then(function(dataUrl) {
+        var link = document.createElement('a')
+        link.href = dataUrl
+        link.download = eventTitle.value + '.jpg'
+        link.click()
+    })
 })
